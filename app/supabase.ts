a61b598b126924921dbc026e019eb4b67084117a
Zachary King <zak@delfina.com>
2025-12-27 09:10:49 -0800

    supabase link

---
+++ app/supabase.ts
@@ -0,0 +1,24 @@
+import 'react-native-url-polyfill/auto';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+if (!supabaseUrl) {
+  throw new Error("Missing Supabase URL");
+}
+
+if (!supabasePublishableKey) {
+  throw new Error("Missing Supabase Publishable Key");
+}
+
+export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
