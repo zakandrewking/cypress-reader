bfe844cdb4557d3eb23a182cfe332b3d7ad0ecfc
Zachary King <zak@delfina.com>
2025-12-28 20:20:16 -0800

    TODOs

--- schema/users.lp.sql
+++ schema/users.lp.sql
@@ -1,8 +1,16 @@
 CREATE TABLE users(
     id bigint NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    -- TODO apply foreign keys
+    user_id uuid REFERENCES auth.users(id),
     name text,
     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 ALTER TABLE users ENABLE ROW LEVEL SECURITY;
 
+-- TODO apply policies
+CREATE POLICY "Users manage their own records" ON users TO authenticated
+    USING (auth.uid() = user_id)
+    WITH CHECK (auth.uid() = user_id);
+
+-- TODO throttle vscode extension activations
